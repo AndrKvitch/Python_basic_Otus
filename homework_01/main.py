711f4349e59f78b92e44bdd3e4cc9ce58de9bcaf
AndrKvitch <102856320+AndrKvitch@users.noreply.github.com>
2023-03-21 12:17:05 +0300

    Add homework_01

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,6 +4,9 @@
 """
 
 
+import sympy
+from sympy import *
+
 def power_numbers(*numbers):
     """
     функция, которая принимает N целых чисел,
@@ -30,10 +33,12 @@ def filter_numbers(numbers_list, filter_type):
     <<< [1, 3]
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
+    >>> filter_numbers([1, 2, 3, 4, 5], PRIME)
     """
     if filter_type == ODD:
-        return [number for number in numbers_list if number % 2 != 0
-                and number % 1 == 0 and number % number == 0]
+        return [number for number in numbers_list if number % 2 != 0]
     if filter_type == EVEN:
         return [number for number in numbers_list if number % 2 == 0]
+    if filter_type == PRIME:
+        return [number for number in numbers_list if isprime(number)]
 
